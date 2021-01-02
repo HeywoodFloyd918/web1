418302323c3358009b5d2687dd7285d6e63cd389
egor <piratikcooldog@gmail.com>
2021-01-02 06:03:38 +0300

    finally add new button and increase by 2

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let btn = document.querySelector("button");
+let btnM = document.querySelectorAll("button")[0];
+let btnP = document.querySelectorAll("button")[1];
 let h3 = document.querySelector("h3");
 let count = 0;
-btn.onclick = ()=>{
-    h3.innerHTML = ++count;
+btnM.onclick = ()=>{
+    count-=2;
+    h3.innerHTML = count;
+}
+btnP.onclick = ()=>{
+    count+=2;
+    h3.innerHTML = count;
 }
